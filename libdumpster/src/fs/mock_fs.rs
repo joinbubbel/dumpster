4479d9fa8a9dcf67c8ae8759fd9877543780b14e
dav <usr21p1@gmail.com>
2023-08-28 19:12:12 -0700

    Dumpster Mock and Tokio fs

---
+++ libdumpster/src/fs/mock_fs.rs
@@ -0,0 +1,83 @@
+use super::*;
+use std::{collections::HashMap, sync::Mutex};
+
+#[derive(Default)]
+pub struct MockFileSystem {
+    classes: Mutex<HashMap<String, Class>>,
+}
+
+#[derive(Default)]
+struct Class {
+    objects: HashMap<String, Object>,
+}
+
+#[derive(Default)]
+struct Object {
+    datas: HashMap<String, Vec<u8>>,
+}
+
+#[async_trait]
+impl FileSystem for MockFileSystem {
+    async fn mount(_mount_point: &str) -> Result<Self, FileSystemError>
+    where
+        Self: Sized,
+    {
+        Ok(MockFileSystem::default())
+    }
+
+    async fn register_class(&self, name: &str) -> Result<(), FileSystemError> {
+        let mut classes = self.classes.lock().unwrap();
+        classes.insert(name.to_owned(), Class::default());
+        Ok(())
+    }
+
+    async fn new_object(&self, class_name: &str, object_name: &str) -> Result<(), FileSystemError> {
+        let mut classes = self.classes.lock().unwrap();
+        classes
+            .get_mut(class_name)
+            .unwrap()
+            .objects
+            .insert(object_name.to_owned(), Object::default());
+        Ok(())
+    }
+
+    async fn load(
+        &self,
+        class_name: &str,
+        object_name: &str,
+        data_name: &str,
+    ) -> Result<Vec<u8>, FileSystemError> {
+        let mut classes = self.classes.lock().unwrap();
+        Ok(classes
+            .get_mut(class_name)
+            .unwrap()
+            .objects
+            .get_mut(object_name)
+            .unwrap()
+            .datas
+            .get(data_name)
+            .unwrap()
+            .clone())
+    }
+
+    async fn store(
+        &self,
+        class_name: &str,
+        object_name: &str,
+        name: &str,
+        bytes: &[u8],
+    ) -> Result<(), FileSystemError> {
+        let mut classes = self.classes.lock().unwrap();
+        *classes
+            .get_mut(class_name)
+            .unwrap()
+            .objects
+            .get_mut(object_name)
+            .unwrap()
+            .datas
+            .get_mut(name)
+            .unwrap() = bytes.to_vec();
+
+        Ok(())
+    }
+}
---
+++ libdumpster/src/fs/tokio_fs.rs
@@ -0,0 +1,74 @@
+use super::*;
+use std::{io, path::PathBuf};
+use tokio::fs;
+
+pub struct TokioFileSystem {
+    mount_point: PathBuf,
+}
+
+#[async_trait]
+impl FileSystem for TokioFileSystem {
+    async fn mount(mount_point: &str) -> Result<Self, FileSystemError>
+    where
+        Self: Sized,
+    {
+        let mount_point = PathBuf::from(mount_point);
+        Ok(Self { mount_point })
+    }
+
+    async fn register_class(&self, name: &str) -> Result<(), FileSystemError> {
+        let mut dir = self.mount_point.clone();
+        dir.push(name);
+        fs::create_dir(&dir)
+            .await
+            .map_err(|e| FileSystemError::Internal(e.to_string()))
+    }
+
+    async fn new_object(&self, class_name: &str, object_name: &str) -> Result<(), FileSystemError> {
+        let mut dir = self.mount_point.clone();
+        dir.push(class_name);
+        dir.push(object_name);
+        fs::create_dir(&dir)
+            .await
+            .map_err(|e| FileSystemError::Internal(e.to_string()))
+    }
+
+    async fn load(
+        &self,
+        class_name: &str,
+        object_name: &str,
+        data_name: &str,
+    ) -> Result<Vec<u8>, FileSystemError> {
+        let mut dir = self.mount_point.clone();
+        dir.push(class_name);
+        dir.push(object_name);
+        dir.push(data_name);
+        fs::read(&dir).await.map_err(|e| {
+            if e.kind() == io::ErrorKind::NotFound {
+                FileSystemError::NotFound
+            } else {
+                FileSystemError::Internal(e.to_string())
+            }
+        })
+    }
+
+    async fn store(
+        &self,
+        class_name: &str,
+        object_name: &str,
+        name: &str,
+        bytes: &[u8],
+    ) -> Result<(), FileSystemError> {
+        let mut dir = self.mount_point.clone();
+        dir.push(class_name);
+        dir.push(object_name);
+        dir.push(name);
+        fs::write(&dir, bytes).await.map_err(|e| {
+            if e.kind() == io::ErrorKind::NotFound {
+                FileSystemError::NotFound
+            } else {
+                FileSystemError::Internal(e.to_string())
+            }
+        })
+    }
+}
